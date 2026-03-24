6ee00fec3812b562e52a856e321c44f93bc01b58
Nocodenobug <kphongtq12082007@gmail.com>
2026-03-24 21:40:49 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        User u1 = new User("Phong");
+        User u1 = new User("Tue");
         System.out.println(u1.getName());
     }
 }
