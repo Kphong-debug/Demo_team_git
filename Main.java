59463553dbb980839dd3e41009da0c55f6fb58e3
Hoàng Hà Anh <intotheunknownohhh@gmail.com>
2026-03-24 22:12:10 +0700

    commit

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        User u1 = new User("Phong");
+        User u1 = new User("fuck up");
         System.out.println(u1.getName());
     }
 }
